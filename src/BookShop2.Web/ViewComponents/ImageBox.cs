eb2c7b6868dea87b3118c7a75959b8544621ac0d
Parsa Maghsodian <parsa.maghsodian@gmail.com>
2025-03-18 22:52:23 +0330

    Added ImageBox ViewComponent for Base64Convertion

---
+++ src/BookShop2.Web/ViewComponents/ImageBox.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop2.Web.ViewComponents;
+
+[ViewComponent]
+public class ImageBox : ViewComponent
+{
+    public IViewComponentResult Invoke(byte[] ? contents, string style, string alt , string classboot)
+    {
+        var viewModel = new ImageBoxViewModel
+        {
+            ImageBase64 = contents != null ? Convert.ToBase64String(contents) : null,
+            Style = style,
+            Alt = alt , 
+            Class = classboot
+        };
+        return View("Default",viewModel);
+    }
+  
+}
+
+
+public class ImageBoxViewModel
+{
+    public string  ? ImageBase64 { get; set; }
+    public required string  Style { get; set; }
+    public required string Alt { get; set; }
+    public required string Class { get; set; }
+}
