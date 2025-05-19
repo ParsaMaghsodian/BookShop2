af7feabcfe87bf686d9cf358017a1bab5e27b244
Parsa Maghsodian <parsa.maghsodian@gmail.com>
2025-05-19 22:44:13 +0330

    Sepreted Create file logic operation from Uploading file by admin and added into fileservice

--- src/BookShop2/Application/Interfaces/IFileService.cs
+++ src/BookShop2/Application/Interfaces/IFileService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,4 +14,5 @@ public interface IFileService
     Task<(byte[] content, string fileName)?> GetUserBookFileAsync(string userId, int bookId); // User use
     bool DeleteFile(string filename);
     IEnumerable<FileInfo> GetAllFiles();
+    Task<bool> CreateFileAsync(IFormFile uploadedFile);
 }
